97bafb5819ae9fb012ca5dbc7a08deea4b448918
omar ashraf <moryasfraf@gmail.com>
2025-09-26 19:46:04 +0300

    Add files via upload

---
+++ singlton excer/singlton excer/singlton excer.cpp
@@ -0,0 +1,43 @@
+
+#include <iostream>
+#include<thread>
+#include<mutex>
+#define null NULL
+using namespace std;
+class counter {
+    int count = 0;
+    counter() {};
+    counter(const counter&) = delete;
+    counter& operator = (const counter&) = delete;
+    static counter* inst;
+    static mutex mut;
+
+    void addone();
+public:
+    static counter* getinst(){
+        if (inst == null) {
+            lock_guard<mutex>lock(mut);
+            if (inst == nullptr) {
+                inst = new counter();
+            }
+}
+    return inst;
+    }
+  
+    void incrment(){ addone(); }
+    void show() { cout << "Count: "<< count << endl; }
+};
+counter* counter::inst = nullptr;
+mutex counter::mut;
+
+void counter::addone() {
+    count++;
+}
+int main()
+{
+    counter* c1 = counter::getinst();
+    c1->incrment();
+    c1->show();
+    return 0;
+}
+
